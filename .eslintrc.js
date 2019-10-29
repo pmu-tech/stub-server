df74df14747af3cc97277033bc7463a6910a671b
Tanguy Krotoff <tkrotoff@gmail.com>
2019-10-29 17:47:12 +0100

    Project scaffolding

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {},
+  extends: [
+    // /!\ Order matters
+
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
---
+++ README.md
@@ -0,0 +1,18 @@
+# Stub server
+
+Stub server for REST APIs
+
+## Usage
+
+`npm install --save-dev @pmu-tech/stub-server`
+
+## Contributing
+
+### Prerequisites
+
+- Node.js >= 10
+- VSCode extensions: "EditorConfig for VS Code" and "Prettier - Code formatter"
+
+### Build
+
+`npm run build`
