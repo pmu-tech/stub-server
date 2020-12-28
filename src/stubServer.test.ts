558f09845f405d59a5723f1dceaf0b1e207e0145
Tanguy Krotoff <tkrotoff@gmail.com>
2020-12-28 22:29:50 +0100

    Fix typo

--- src/stubServer.test.ts
+++ src/stubServer.test.ts
@@ -377,7 +377,7 @@ describe('express request handler', () => {
     });
 
     // eslint-disable-next-line jest/expect-expect
-    test('do nothing"', async () => {
+    test('do nothing', async () => {
       // Timeout - Async callback was not invoked within the 5000ms timeout specified by jest.setTimeout.Timeout
       // await request(app).get('/get/express/ts/doNothing');
     });
