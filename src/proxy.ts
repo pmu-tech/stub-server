6a7273d0ed838327ef1d2a5e2c88ea11971ff024
Tanguy Krotoff <tkrotoff@gmail.com>
2021-11-15 18:02:26 +0100

    Remove obsolete comment

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,8 +3,6 @@ import { createProxyServer } from 'http-proxy';
 
 const proxy = createProxyServer({ changeOrigin: true });
 
-// Exported for testing purposes only, see [Jest mock inner function](https://stackoverflow.com/q/51269431)
-
 export const send = (
   target: string,
   req: express.Request,
