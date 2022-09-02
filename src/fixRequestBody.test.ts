ad620957dd0349539683912f0b9513cedf7c6d3e
Tanguy Krotoff <tkrotoff@gmail.com>
2022-09-02 11:46:45 +0200

    Fix "A worker process has failed to exit gracefully and has been force exited"
    Detected via Jest option "--detectOpenHandles"

--- src/fixRequestBody.test.ts
+++ src/fixRequestBody.test.ts
@@ -1,16 +1,24 @@
+/* eslint-disable jest/no-done-callback */
+
 import express from 'express';
 import http from 'http';
 
 import { fixRequestBody } from './fixRequestBody';
 
+function noop() {
+  // No operation performed
+}
+
 const fakeProxyRequest = () => {
   const proxyRequest = new http.ClientRequest('http://some-host');
-  proxyRequest.emit = () => false; // Otherwise we get "Error: getaddrinfo ENOTFOUND some-host"
+
+  // Otherwise we get "Error: getaddrinfo ENOTFOUND some-host"
+  proxyRequest.on('error', noop);
 
   return proxyRequest;
 };
 
-test('should not write when body is undefined', () => {
+test('should not write when body is undefined', done => {
   const proxyRequest = fakeProxyRequest();
 
   jest.spyOn(proxyRequest, 'setHeader');
@@ -20,9 +28,11 @@ test('should not write when body is undefined', () => {
 
   expect(proxyRequest.setHeader).not.toHaveBeenCalled();
   expect(proxyRequest.write).not.toHaveBeenCalled();
+
+  proxyRequest.on('close', done);
 });
 
-test('should not write when body is empty', () => {
+test('should not write when body is empty', done => {
   const proxyRequest = fakeProxyRequest();
 
   jest.spyOn(proxyRequest, 'setHeader');
@@ -32,9 +42,11 @@ test('should not write when body is empty', () => {
 
   expect(proxyRequest.setHeader).not.toHaveBeenCalled();
   expect(proxyRequest.write).not.toHaveBeenCalled();
+
+  proxyRequest.on('close', done);
 });
 
-test('should write when body is not empty and Content-Type is application/json', () => {
+test('should write when body is not empty and Content-Type is application/json', done => {
   const proxyRequest = fakeProxyRequest();
   proxyRequest.setHeader('content-type', 'application/json; charset=utf-8');
 
@@ -46,9 +58,11 @@ test('should write when body is not empty and Content-Type is application/json',
   const expectedBody = JSON.stringify({ someField: 'some value' });
   expect(proxyRequest.setHeader).toHaveBeenCalledWith('Content-Length', expectedBody.length);
   expect(proxyRequest.write).toHaveBeenCalledWith(expectedBody);
+
+  proxyRequest.on('close', done);
 });
 
-test('should write when body is not empty and Content-Type is application/x-www-form-urlencoded', () => {
+test('should write when body is not empty and Content-Type is application/x-www-form-urlencoded', done => {
   const proxyRequest = fakeProxyRequest();
   proxyRequest.setHeader('content-type', 'application/x-www-form-urlencoded');
 
@@ -60,4 +74,6 @@ test('should write when body is not empty and Content-Type is application/x-www-
   const expectedBody = 'someField=some+value';
   expect(proxyRequest.setHeader).toHaveBeenCalledWith('Content-Length', expectedBody.length);
   expect(proxyRequest.write).toHaveBeenCalledWith(expectedBody);
+
+  proxyRequest.on('close', done);
 });
